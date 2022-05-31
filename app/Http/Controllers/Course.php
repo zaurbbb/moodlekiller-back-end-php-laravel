987e87b4d4871c771cb5e9e6da8a66a9454fcab8
zaurbbb <zaurberdibekov04@gmail.com>
2022-06-01 00:53:39 +0600

    deploying...

--- app/Http/Controllers/Course.php
+++ app/Http/Controllers/Course.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Course extends \TCG\Voyager\Models\Course
+class Course extends \TCG\Voyager\Models\User
 {
     use HasApiTokens, HasFactory, Notifiable;
 }
