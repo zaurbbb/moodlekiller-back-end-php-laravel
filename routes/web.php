eb435469131a18c1f5b6eb7ad13a94d6e09e56ab
zaurbbb <96561371+zaurbbb@users.noreply.github.com>
2022-07-06 11:51:28 +0600

    Update web.php

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadImageController;
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Auth::routes();
+
+
+
+//For adding an image
+Route::get('/add-image',[UploadImageController::class,'addImage'])->name('Image.add');
+
+//For storing an image
+Route::post('/store-image',[UploadImageController::class,'storeImage'])
+    ->name('Image.store');
+
+//For showing an image
+Route::get('/view-image',[UploadImageController::class,'viewImage'])->name('Image.view');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/courses/grades', 'App\Http\Controllers\PagesController@grades');
+Route::get('/courses/attendance', 'App\Http\Controllers\PagesController@attendance');
+Route::get('/courses/schedule', 'App\Http\Controllers\PagesController@schedule');
+Route::get('/info', 'App\Http\Controllers\PagesController@info');
+Route::get('/profile', 'App\Http\Controllers\PagesController@profile');
